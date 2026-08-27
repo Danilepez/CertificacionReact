788f04cf342a6dff6f2cb258813cd3ce7e8eff01
Danilepez <danilepez5@gmail.com>
2026-08-27 18:31:17 -0400

    clase18

---
+++ clase18/tres-en-raya/src/App.jsx
@@ -0,0 +1,6 @@
+import {createRoot} from 'react-dom/client';
+import TresEnRaya from './tres-en-raya.jsx';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<TresEnRaya />);
