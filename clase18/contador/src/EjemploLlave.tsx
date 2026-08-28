a351bc4c8d1d898f36c05b236fe73b960cb5b4a0
Danilepez <danilepez5@gmail.com>
2026-08-28 18:43:56 -0400

    clase 19

--- clase18/contador/src/EjemploLlave.tsx
+++ clase18/contador/src/EjemploLlave.tsx
@@ -3,7 +3,7 @@ export default function EjemploLlave() {
       return a + b
     }
     const resultado = suma(3, 4)
-    const mensaje: string = El resultado de la suma es: ${resultado}   
+    const mensaje: string = `El resultado de la suma es: ${resultado} `;
   return (
     <section>
         <h1>Ejemplo de uso de llaves en JSX</h1>
